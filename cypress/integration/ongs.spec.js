5c6891cb4854345487ea5b33263d9c022bcbe8d5
jmmtreinamento <jmmtreinamento@gmail.com>
2022-02-22 16:06:59 -0300

    cadastro e edição do edital

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -10,7 +10,52 @@ describe('PNLD', () => {
     });
 
 
-    it('deve navegar nos menus superiores do sistema', () => {
+    it(' clicar no botão cadastrar edital ', () => {
         cy.wait(1000);
+        cy.get('.MuiGrid-root > .MuiButtonBase-root > .MuiButton-label').click();
+      });
+
+    it(' preencher a tela de cadatro ', () => {
+        cy.wait(1000);
+        cy.get('#title').type("treino22");
+        cy.get('#year').type("2022");
+        cy.get('#type').click();
+        cy.wait(900);
+        cy.get('[data-value="PNLD"] > .MuiListItemText-root > .MuiTypography-root').click();
+        cy.get('#number').type("02");
+      });
+
+    /*it(' clicar no botão cadastrar na tela de cadatro ', () => {
+        cy.wait(3000);
+        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click();
+      });*/
+
+    it(' clicar no X na tela de cadastro ', () => {
+        cy.wait(1000);
+        cy.get('.MuiDialogTitle-root > div > .MuiButtonBase-root > .MuiIconButton-label > svg > path').click();
+       });
+
+    /*it(' clicar em perfil no topo da tela ', () => {
+        cy.wait(1000);
+        cy.get('.sc-fFucqa > .MuiButtonBase-root > .MuiButton-label > span').click();
     });
+
+    it('pesquisar por edital ', () => {
+        cy.wait(3000);
+        cy.get('#searchTerm').click();
+        cy.get('#searchTerm').type("JMM EDITAL TESTE 21/02/2022");
+        cy.get('#btSearch > .MuiIconButton-label > .MuiSvgIcon-root').click();
+            });*/
+
+    it(' expandir informações do edital ', () => {
+        cy.wait(1000);
+        cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(5) > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root').click();
+        });
+
+    it('editar o edital ', () => {
+        cy.wait(3000);
+        cy.get('.MuiTableCell-alignCenter > .MuiButtonBase-root > .MuiIconButton-label > svg > path').click();
+        cy.get('.MuiList-root > [tabindex="0"]').click();
+        });
+    
 });
