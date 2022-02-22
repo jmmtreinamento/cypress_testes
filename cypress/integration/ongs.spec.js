58d741c87c6cc9d6e3433721256c450091d4c757
jmmtreinamento <jmmtreinamento@gmail.com>
2022-02-22 16:20:32 -0300

    new teste

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -9,8 +9,31 @@ describe('PNLD', () => {
         cy.get('#btn-signin > .MuiButton-label').click();
     });
 
+    it(' Verificar se está na tela de recursos ', () => {
+        cy.wait(2000)  // 2 segundos
+        cy.get('[href="/appeals"]').click();
 
-    it('deve navegar nos menus superiores do sistema', () => {
-        cy.wait(1000);
     });
+
+    /*it(' Verificar se está na tela de Usuários ', () => {
+        cy.wait(2000)  // 2 segundos
+        cy.get('[href="/users-list"]').click();
+    });*/
+
+    it(' Verificar se está na tela de Solicitações ', () => {
+        cy.wait(2000)  // 2 segundos
+        cy.get('[href="/publisher-request"]').click();
+    });
+
+    it(' Verificar se está na tela de Alertas ', () => {
+        cy.wait(2000)  // 2 segundos
+        cy.get('[href="/alert"]').click();
+    
+    });
+
+    it(' Verificar se está na tela de Configuraçõe ', () => {
+        cy.wait(2000)  // 2 segundos
+        cy.get('[href="/settings/static-fields"]').click();
+    });
+
 });
